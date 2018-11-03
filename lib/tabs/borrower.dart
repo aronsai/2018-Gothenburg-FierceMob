366f002498919cd42a02d90c2fc62613b6c47cb6
Aron Sai <aron.sai@gmail.com>
2018-11-03 19:31:33 +0100

    Final fixes for Borrower view

--- lib/tabs/borrower.dart
+++ lib/tabs/borrower.dart
@@ -5,7 +5,7 @@ enum Category { tools, all }
 class Borrower extends StatelessWidget {
   @override
   Widget build(BuildContext context) => new Container(
-      child: new Column(
+      child: new ListView(
         children: <Widget>[
           Padding(
             padding: const EdgeInsets.all(20.0),
@@ -39,47 +39,73 @@ class Borrower extends StatelessWidget {
               ],
             ),
           ),
-          new ListView(
-            shrinkWrap: true,
-            padding: const EdgeInsets.all(20.0),
-            children: <Widget>[
-              Card(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    ListTile(
-                      leading: new CircleAvatar(
-                          child: new Image.asset('lib/src/powerdrill.jpg')
-                      ),
-                      title: Text('DEWALT DC759KA'),
-                      subtitle: Text(
-                          'My power drill! It works great for everyday stuff.'),
-                    ),
-                    ButtonTheme
-                        .bar( // make buttons use the appropriate styles for cards
-                      child: ButtonBar(
-                        children: <Widget>[
-                          FlatButton(
-                            child: const Text('Borrow it!'),
-                            onPressed: () {
-                              Navigator.pop(context);
-                              Navigator.of(context).pushNamed('/lending');
-
-                              /* ... */
-                            },
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
+          new Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: new Column(
+                children: <Widget>[
+                  createItem('DEWALT DC759KA',
+                      'My power drill! It works great for everyday stuff.',
+                      'Göran', context),
+                  createItem('DEWALT DC759KA',
+                      'My power drill! It works great for everyday stuff.',
+                      'Göran', context),
+                  createItem('DEWALT DC759KA',
+                      'My power drill! It works great for everyday stuff.',
+                      'Göran', context),
+                  createItem('DEWALT DC759KA',
+                      'My power drill! It works great for everyday stuff.',
+                      'Göran', context),
+                  createItem('DEWALT DC759KA',
+                      'My power drill! It works great for everyday stuff.',
+                      'Göran', context),
+                  createItem('DEWALT DC759KA',
+                      'My power drill! It works great for everyday stuff.',
+                      'Göran', context),
+                ],
               )
-            ],
           )
         ],
       )
 
   );
+
+  Card createItem(String title, String subtitle, String owner,
+      BuildContext context) {
+    Card item = new Card(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          ListTile(
+            leading: new CircleAvatar(
+                child: new Image.asset('lib/src/powerdrill.jpg')
+            ),
+            title: Text(title),
+            subtitle: Text(
+                subtitle),
+          ),
+          ButtonTheme
+              .bar( // make buttons use the appropriate styles for cards
+            child: ButtonBar(
+              children: <Widget>[
+                Text('Owned by ' + owner),
+                FlatButton(
+                  child: const Text('Borrow it!'),
+                  onPressed: () {
+                    Navigator.pop(context);
+                    Navigator.of(context).pushNamed('/lending');
+
+                    /* ... */
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+    return item;
+  }
+
 }
 
 
@@ -113,5 +139,4 @@ class _CategorySelectorState extends State<CategorySelector> {
       ],
     );
   }
-
 }
